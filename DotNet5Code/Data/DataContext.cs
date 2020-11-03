61d2924d1c0bfd3a078306e885f6931f4354b9df
umeshdhital <umeshdhital@gmail.com>
2020-11-02 21:44:12 -0500

    Adding DBSet

---
+++ DotNet5Code/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNet5Code.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet5Code.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        private DbSet<Contact> Contracts { get; set; }
+    }
+}
---
+++ DotNet5Code/Model/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5Code.Model
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; }= DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
